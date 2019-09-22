a87cdb6d0ca597622b9156591038214db17ca231
Yvictor <410175015@gms.ndhu.edu.tw>
2019-09-22 14:10:22 +0800

    clean fat code

--- authentication_service.py
+++ authentication_service.py
@@ -1,6 +1,12 @@
 import sqlite3
 import hashlib
 import http.client
+import logging
+
+
+class FailedTooManyTimesException(Exception):
+    pass
+
 
 class AuthenticationService:
 
@@ -8,21 +14,37 @@ class AuthenticationService:
         pass
 
     def verify(self, account_id: str, password: str, otp: str) -> bool:
+        http_client = http.client.HTTPSConnection("yvictor.com")
+        response = http_client.request("POST", "api/failedCounter/IsLocked", account_id)
+        if response.plain_text == "true":
+            raise FailedTooManyTimesException()
+
         connect = sqlite3.connect('connecthost')
         password1 = connect.execute(f"spGetUserPassword{account_id}").fetchone()
         password_from_db = password1
         hashobj = hashlib.sha256(password.encode('utf8'))
         hashed_password = hashobj.hexdigest()
-        conn = http.client.HTTPSConnection("yvictor.com")
-        response = conn.request("POST", "/api/otps", account_id).getresponse()
+        response = http_client.request("POST", "/api/otps", account_id).getresponse()
         if response.status == 200:
             result = response.plain_text
         else:
             raise Exception(f"web api error, accountId:{account_id}")
         current_opt = result
         if (password_from_db == hashed_password) and (current_opt == otp):
+            http_client.request("POST", "api/failedCounter/Reset", account_id)
             return True
         else:
+            http_client.request("POST", "api/failedCounter/Add", account_id)
+
+            response = http_client.request("POST", "api/failedCounter/GetFailedCount", account_id)
+            failed_count = int(response.plain_text)
+            logger = logging.getLogger(__class__)
+            logger.info(f"accountId:{account_id} failed times:{failed_count}")
+
+            message = f"{account_id} try to login failed"
+            slack_client = SlackClient('api token')
+            slack_client.post_message("my channel", message, "my bot name")
+
             return False
 
 
