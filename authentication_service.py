f4d9093d9ef5a3363eea9a71f7fe5d46ac1b74a3
Yvictor <410175015@gms.ndhu.edu.tw>
2019-09-22 15:33:06 +0800

    extract method

--- authentication_service.py
+++ authentication_service.py
@@ -15,37 +15,67 @@ class AuthenticationService:
 
     def verify(self, account_id: str, password: str, otp: str) -> bool:
         http_client = http.client.HTTPSConnection("yvictor.com")
-        response = http_client.request("POST", "api/failedCounter/IsLocked", account_id)
-        if response.plain_text == "true":
+        is_locked = self.get_is_locked(account_id, http_client)
+        if is_locked:
             raise FailedTooManyTimesException()
 
-        connect = sqlite3.connect('connecthost')
-        password1 = connect.execute(f"spGetUserPassword{account_id}").fetchone()
-        password_from_db = password1
-        hashobj = hashlib.sha256(password.encode('utf8'))
-        hashed_password = hashobj.hexdigest()
-        response = http_client.request("POST", "/api/otps", account_id).getresponse()
-        if response.status == 200:
-            result = response.plain_text
-        else:
-            raise Exception(f"web api error, accountId:{account_id}")
-        current_opt = result
+        password_from_db = self.get_password_from_db(account_id)
+        hashed_password = self.get_hashed_password(password)
+        current_opt = self.get_current_otp(account_id, http_client)
         if (password_from_db == hashed_password) and (current_opt == otp):
-            http_client.request("POST", "api/failedCounter/Reset", account_id)
+            self.reset_failed_count(account_id, http_client)
             return True
         else:
-            http_client.request("POST", "api/failedCounter/Add", account_id)
+            self.add_failed_count(account_id, http_client)
 
-            response = http_client.request("POST", "api/failedCounter/GetFailedCount", account_id)
-            failed_count = int(response.plain_text)
-            logger = logging.getLogger(__class__)
-            logger.info(f"accountId:{account_id} failed times:{failed_count}")
+            self.log_failed_count(account_id, http_client)
 
-            message = f"{account_id} try to login failed"
-            slack_client = SlackClient('api token')
-            slack_client.post_message("my channel", message, "my bot name")
+            self.notify(account_id)
 
             return False
+
+    def reset_failed_count(self, account_id, http_client):
+        http_client.request("POST", "api/failedCounter/Reset", account_id)
+
+    def get_is_locked(self, account_id, http_client):
+        response = http_client.request("POST", "api/failedCounter/IsLocked", account_id)
+        is_locked = response.plain_text == "true"
+        return is_locked
+
+    def notify(self, account_id):
+        message = f"{account_id} try to login failed"
+        slack_client = SlackClient('api token')
+        slack_client.post_message("my channel", message, "my bot name")
+
+    def log_failed_count(self, account_id, http_client):
+        failed_count = self.get_failed_count(account_id, http_client)
+        logger = logging.getLogger(__class__)
+        logger.info(f"accountId:{account_id} failed times:{failed_count}")
+
+    def get_failed_count(self, account_id, http_client):
+        response = http_client.request("POST", "api/failedCounter/GetFailedCount", account_id)
+        failed_count = int(response.plain_text)
+        return failed_count
+
+    def add_failed_count(self, account_id, http_client):
+        http_client.request("POST", "api/failedCounter/Add", account_id)
+
+    def get_current_otp(self, account_id, http_client):
+        response = http_client.request("POST", "/api/otps", account_id).getresponse()
+        if response.status != 200:
+            raise Exception(f"web api error, accountId:{account_id}")
+        current_opt = response.plain_text
+        return current_opt
+
+    def get_hashed_password(self, password: str) -> str:
+        hashobj = hashlib.sha256(password.encode('utf8'))
+        hashed_password = hashobj.hexdigest()
+        return hashed_password
+
+    def get_password_from_db(self, account_id: str) -> str:
+        connect = sqlite3.connect('connecthost')
+        password_from_db = connect.execute(f"spGetUserPassword{account_id}").fetchone()
+        return password_from_db
 
 
 if __name__ == "__main__":
